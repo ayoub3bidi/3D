bc34a3e8530f07d2c45f3e758e00006fecd84582
ayoub3bidi <mrayoubabidi@gmail.com>
2023-05-14 15:11:39 +0100

    feat: create a scene & add camera

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,32 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75, 
+    window.innerWidth / window.innerHeight,
+    0.1, 
+    1000
+);
+
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+camera.position.set(0, 2, 5);
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+function animate(time) {
+    box.rotation.x = time/1000;
+    box.rotation.y = time/1000;
+
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+renderer.render(scene, camera);
